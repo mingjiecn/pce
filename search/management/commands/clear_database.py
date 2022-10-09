c479da9932accff5297449e3f14fa8585bba1bc3
Mingjie Li <mingjiecn@gmail.com>
2022-10-09 11:41:59 -0500

    update clear database command

--- search/management/commands/clear_database.py
+++ search/management/commands/clear_database.py
@@ -1,39 +1,42 @@
 import csv
 from django.core.management import BaseCommand
-from search.models import Inguinal_male_protein_1, Inguinal_male_rna_2, Inguinal_female_protein_3, Inguinal_female_rna_4
-from search.models import Perigonadal_male_protein_5, Perigonadal_male_rna_6, Perigonadal_female_protein_7, Perigonadal_female_rna_8
-from search.models import Sex_fip_protein_9, Sex_fip_rna_10, Sex_apc_protein_11, Sex_apc_rna_12, Sex_dpp4_positive_protein_13, Sex_dpp4_positive_rna_14
-from search.models import Sex_dpp4_neg_protein_15, Sex_dpp4_neg_rna_16, Depot_male_protein_17, Depot_male_rna_18, Depot_female_protein_19, Depot_female_rna_20
+from search.models import Patient, Pathology_TR, Procedure, RadiationFraction, Lab, ChemoPlan, ChemoCycle, RadiationSet
+
 import os
 
 class Command(BaseCommand):
     help = 'Load a questions csv file into the database'
 
     def add_arguments(self, parser):
-        parser.add_argument('--path', type=str)
+        parser.add_argument('--model', type=str)
 
     def handle(self, *args, **kwargs):
 
-        Inguinal_male_protein_1.objects.all().delete()    
-        Inguinal_male_rna_2.objects.all().delete()
-        Inguinal_female_protein_3.objects.all().delete()
-        Inguinal_female_rna_4.objects.all().delete()
-        Perigonadal_male_protein_5.objects.all().delete()
-        Perigonadal_male_rna_6.objects.all().delete()
-        Perigonadal_female_protein_7.objects.all().delete()
-        Perigonadal_female_rna_8.objects.all().delete()
-        Sex_fip_protein_9.objects.all().delete()
-        Sex_fip_rna_10.objects.all().delete()
-        Sex_apc_protein_11.objects.all().delete()
-        Sex_apc_rna_12.objects.all().delete()
-        Sex_dpp4_positive_protein_13.objects.all().delete()
-        Sex_dpp4_positive_rna_14.objects.all().delete()
-        Sex_dpp4_neg_protein_15.objects.all().delete()
-        Sex_dpp4_neg_rna_16.objects.all().delete()
-        Depot_male_protein_17.objects.all().delete()
-        Depot_male_rna_18.objects.all().delete()
-        Depot_female_protein_19.objects.all().delete()
-        Depot_female_rna_20.objects.all().delete()
+        if kwargs['model'] == 'Patient':
+            Patient.objects.all().delete()
+            print("Patient data is deleted")
+        elif kwargs['model'] == 'Pathology_TR':
+            Pathology_TR.objects.all().delete()
+            print("Pathology_TR data is deleted")
+        elif kwargs['model'] == 'Procedure':
+            Procedure.objects.all().delete()
+            print("Procedure data is deleted")
+        elif kwargs['model'] == 'RadiationFraction':
+            RadiationFraction.objects.all().delete()
+            print("RadiationFraction data is deleted")
+        elif kwargs['model'] == 'Lab':
+            Lab.objects.all().delete()
+            print("Lab data is deleted")
+        elif kwargs['model'] == 'ChemoPlan':
+            ChemoPlan.objects.all().delete()
+            print("ChemoPlan data is deleted")
+        elif kwargs['model'] == 'ChemoCycle':
+            ChemoCycle.objects.all().delete()
+            print("ChemoCycle data is deleted")
+        elif kwargs['model'] == 'RadiationSet':
+            RadiationSet.objects.all().delete()
+            print("RadiationSet data is deleted")
+
 
                 
                 
